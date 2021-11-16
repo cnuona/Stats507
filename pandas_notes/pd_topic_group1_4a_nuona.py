de75358d08cee7e2ce2806fb5dd6d8260803372b
Nuona Chen <nuonachen@Nuonas-MacBook-Air.local>
2021-11-15 20:44:56 -0500

    add part3_q2

---
+++ pandas_notes/pd_topic_group1_4a_nuona.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+# Sorting in Python Pandas
+
+# ## Contents
+# + [Sorting in Python](#https://github.com/cnuona/Stats507/blob/main/pandas_notes/pd_topic_nuona.py) 
+# + [Topic 2 Title](#Topic-2-Title)
+
+# ## Topic Title
+# <h2> Sorting in Python Pandas</h2>
+# <h3>DataFrame.sort_values()</h3>
+# <p> <b> Nuona Chen </b></p>
+
